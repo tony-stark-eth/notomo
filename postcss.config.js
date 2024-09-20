59526cea0480cf15914a0ea98ea0672645ce8eea
Kevin Mauel <kevin.mauel2+github@gmail.com>
2024-09-20 16:39:54 +0200

    feat: add tailwindcss

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+import process from 'node:process';
+
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
+  },
+};
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import '../app.css';
+
+  const { children } = $props();
+</script>
+
+<main>
+  {@render children()}
+</main>
